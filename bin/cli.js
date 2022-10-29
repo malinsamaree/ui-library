6697186d9bafd1a2a313f4f6e6a1542147ba42a1
malinsamaree <malinsamare@gmail.com>
2022-10-29 13:37:57 +0200

    added npx commands

---
+++ bin/cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const {execSync} = require('child_process');
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, {stdio: 'inherit'});
+    } catch (e) {
+        console.error(`Failed to execute ${command}`, e);
+        return false;
+    }
+    return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/malinsamaree/npm-hello-world ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository - ${repoName}`);
+
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if(!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies`);
+
+const installedDeps = runCommand(installDepsCommand);
+
+if(!installedDeps) process.exit(-1);
+
+console.log("Happy hacking!!!");
