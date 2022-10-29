c023762f5d96594414f3e0edd0c0156d314fa34f
malinsamaree <malinsamare@gmail.com>
2022-10-29 13:56:24 +0200

    added npx commands

--- bin/cli.js
+++ bin/cli.js
@@ -13,7 +13,7 @@ const runCommand = command => {
 };
 
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/malinsamaree/npm-hello-world ${repoName}`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/malinsamaree/ui-library ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(`Cloning the repository - ${repoName}`);
