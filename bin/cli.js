6d8e96124f0d4105baec4c27ca23f571cdee5261
malinsamaree <malinsamare@gmail.com>
2022-11-02 19:19:45 +0100

    adding npx commands - works.... installs succesfully, change package.json and deletes the bin folder

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+
 const fs = require('fs');
+
 const {execSync} = require('child_process');
 
 const runCommand = command => {
@@ -14,26 +16,29 @@ const runCommand = command => {
 
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/malinsamaree/ui-library.git ${repoName}`;
-const cdCommand = `cd ${repoName}`;
-const setNameCommand = `npm pkg set name=${repoName}`;
-const installDepsCommand = `npm install`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+const setupNameCommand = `cd ${repoName} && npm pkg set name=${repoName}`;
+const setupVersionCommand = `cd ${repoName} && npm pkg set version=1.0.0`;
+const setupDeleteBinCommand = `cd ${repoName} && npm pkg delete bin`;
 
 console.log(`Cloning the repository - ${repoName}`);
+
 const checkedOut = runCommand(gitCheckoutCommand);
+
 if(!checkedOut) process.exit(-1);
 
-console.log(`cd'ing in to ${repoName}`);
-const cd = runCommand(cdCommand);
+console.log(`Project setup`)
+runCommand(setupNameCommand);
+runCommand(setupVersionCommand);
+runCommand(setupDeleteBinCommand);
 
-console.log(`Setting up the project`);
-const setName = runCommand(setNameCommand);
-// const packageJsonFileName = `./package.json`;
-// const packageJsonContent = require(packageJsonFileName);
-// packageJsonContent.name = repoName;
-// fs.writeFileSync(packageJsonFileName, JSON.stringify(packageJsonContent));
 
 console.log(`Installing dependencies`);
+
 const installedDeps = runCommand(installDepsCommand);
+
 if(!installedDeps) process.exit(-1);
+
+fs.rmSync(`./${repoName}/bin`, {recursive: true, force: true});
 
 console.log("Happy hacking!!!");
