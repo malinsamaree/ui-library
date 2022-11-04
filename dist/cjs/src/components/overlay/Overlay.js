959eb66abba3004f2b39d01668e8ec7c33a132d5
malinsamaree <malinsamare@gmail.com>
2022-11-04 21:35:01 +0100

    added styled-components@4.4.1 build

--- dist/cjs/src/components/overlay/Overlay.js
+++ dist/cjs/src/components/overlay/Overlay.js
@@ -2,13 +2,17 @@
 
 var index = require('../../../node_modules/react/index.js');
 var style_module = require('./style.module.scss.js');
+var style = require('./style.js');
 
-var Overlay = function Overlay() {
-  return /*#__PURE__*/index.createElement("div", {
+var Overlay = function Overlay(_ref) {
+  var active = _ref.active;
+  return /*#__PURE__*/index.createElement(index.Fragment, null, /*#__PURE__*/index.createElement(style.OverlayHeader, {
+    active: active
+  }, "malin"), /*#__PURE__*/index.createElement("div", {
     className: style_module["overlay-wrapper"]
   }, "overlay", /*#__PURE__*/index.createElement("div", {
     className: "bullet-wrapper"
-  }, "aa"));
+  }, "aa")));
 };
 
 exports.Overlay = Overlay;
